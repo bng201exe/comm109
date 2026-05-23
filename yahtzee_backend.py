58088ed98c9437a0606720f140ca5ab2dae01762
bng201exe <bng201@exeter.ac.uk>
2026-05-23 16:26:48 +0000

    auto-commit: 2026m23-162648

--- yahtzee_backend.py
+++ yahtzee_backend.py
@@ -124,8 +124,8 @@ def score_4_of_a_kind(dice):
 def score_short_straight(dice):
     score = 0
     dice_copy = dice[:]
-    short_straights = ((1,2,3,4), (2,3,4,5), (3,4,5,6))
     short_straight_dice = sorted(set(dice_copy))
+    if short_straight_dice == 
     
     return score
 
