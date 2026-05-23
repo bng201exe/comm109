6ef90166187fb74bf257990a785b496b56c11f9e
bng201exe <bng201@exeter.ac.uk>
2026-05-23 18:39:02 +0000

    auto-commit: 2026m23-183902

--- yahtzee_backend.py
+++ yahtzee_backend.py
@@ -186,7 +186,8 @@ def initialise_game():
 
 def roll_dice(game_state):
     for i in game_state["locked"]:
-            
+            if i == False:
+                
             game_state['dice'][i] = random.randint(1,6)
     return
 
