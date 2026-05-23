3d68ddca7a30dfe4d635e3edf711d411eb1ee771
bng201exe <bng201@exeter.ac.uk>
2026-05-23 17:09:14 +0000

    auto-commit: 2026m23-170914

--- yahtzee_backend.py
+++ yahtzee_backend.py
@@ -173,7 +173,7 @@ def score_yahtzee(dice):
     score = 0
     dice_copy = dice[:]
     dice_set = set(dice_copy)
-    if i == dice_set:
+    if 2 == dice_set:
         score = 50
         break
     return score
