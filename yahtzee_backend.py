0de71a99c41871ee3b3ec6e9f035cdc1bc28976b
bng201exe <bng201@exeter.ac.uk>
2026-05-23 16:33:09 +0000

    auto-commit: 2026m23-163309

--- yahtzee_backend.py
+++ yahtzee_backend.py
@@ -125,6 +125,7 @@ def score_short_straight(dice):
     score = 0
     dice_copy = dice[:]
     short_straight_dice = sorted(set(dice_copy))
+    short_straight1 = 
     if short_straight_dice.pop(1,2,3,4) == True:
         score = 30
     
