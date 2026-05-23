4c59de84673dbdc69445aecded1c5ce5aa7a81a2
bng201exe <bng201@exeter.ac.uk>
2026-05-23 18:22:28 +0000

    auto-commit: 2026m23-182228

--- test_score_full_house.py
+++ test_score_full_house.py
@@ -1,8 +1,8 @@
 from yahtzee_backend import *
 
 
-
-score = score_full_house( [1,2,2,1,1])
-assert score == 25
+def test_score_full_house():
+    score = score_full_house( [1,2,2,1,1])
+    assert score == 20
 
 
