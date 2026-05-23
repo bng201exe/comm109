8078f14cef10834aaa6184a4cff07a465e9538c8
bng201exe <bng201@exeter.ac.uk>
2026-05-23 16:45:21 +0000

    auto-commit: 2026m23-164521

--- yahtzee_backend.py
+++ yahtzee_backend.py
@@ -132,7 +132,8 @@ def score_short_straight(dice):
         score = 30 
     elif (short_straight_dice[0:4] == short_straight2) or (short_straight_dice[1:5] == short_straight2):
         score = 30
-    elif(short_straight_dice[0:4] == short_straight3) or (short_straight_dice[1:5] == short_straight3)
+    elif(short_straight_dice[0:4] == short_straight3) or (short_straight_dice[1:5] == short_straight3):
+        score = 30
     
 
     
