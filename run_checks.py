4c3bf670d0545bf8dc53d64752c8a5b1e7f88915
bng201exe <bng201@exeter.ac.uk>
2026-05-23 16:51:34 +0000

    auto-commit: 2026m23-165134

--- run_checks.py
+++ run_checks.py
@@ -29,8 +29,8 @@ result = score_short_straight(mydice6)
 print(f"running  on {mydice6} ... result is {result}")
 
 mydice7 = [2,2,4,5,6]
-result = score_(mydice6)
-print(f"running  on {mydice6} ... result is {result}")
+result = score_chance(mydice7)
+print(f"running  on {mydice7} ... result is {result}")
 
 #game_state = init_game()
 #game_state['locked'] = []
