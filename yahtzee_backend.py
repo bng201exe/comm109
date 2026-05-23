8521541fb44129f861c9b9a62d2fea16a7221852
bng201exe <bng201@exeter.ac.uk>
2026-05-23 18:58:33 +0000

    auto-commit: 2026m23-185833

--- yahtzee_backend.py
+++ yahtzee_backend.py
@@ -189,6 +189,7 @@ def roll_dice(game_state):
     locked_copy = game_state["locked"[:]]
     locked_copy2 = []
     for i in locked_copy:
+        
         locked_copy2.append(i)
     print(locked_copy)
     print(indices)
