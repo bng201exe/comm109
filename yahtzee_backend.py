5af400c25dedddd788e36675d4de27b3a9c48fc5
bng201exe <bng201@exeter.ac.uk>
2026-05-23 22:30:36 +0000

    auto-commit: 2026m23-223036

--- yahtzee_backend.py
+++ yahtzee_backend.py
@@ -192,6 +192,7 @@ def roll_dice(game_state):
             indices.append(i)
     print(indices)
     for die in game_state["dice"]:
+        roll = ran
         if "dice".index() in indices:
             dice = roll
         
