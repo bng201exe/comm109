84f8e0ea825d63463f439dcbae36fc6374964c67
bng201exe <bng201@exeter.ac.uk>
2026-05-23 18:23:23 +0000

    auto-commit: 2026m23-182323

--- run_checks.py
+++ run_checks.py
@@ -36,7 +36,7 @@ mydice8 = [6,6,6,6,6]
 result = score_yahtzee(mydice8)
 print(f"running  on {mydice8} ... result is {result}")
 
-roll_dice("")
+roll_dice("WAIT_FOR_ROLL")
 
 
 #game_state = init_game()
