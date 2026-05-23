138007f6f9cff3804b03e8b44fae42b6229c32cb
bng201exe <bng201@exeter.ac.uk>
2026-05-23 18:57:06 +0000

    auto-commit: 2026m23-185706

--- run_checks.py
+++ run_checks.py
@@ -43,7 +43,7 @@ game_state = init_game()
 game_state['locked'] = []
 print(game_state)
 roll_dice(game_state)
-#print(game_state)
+print(game_state)
 #display_game_state_debug(game_state)
 #print(game_state['locked'])
 #print(game_state['dice'])
