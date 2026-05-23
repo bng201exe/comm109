42316d4b514a4e4ff28a4cee188f65f0cfb00037
bng201exe <bng201@exeter.ac.uk>
2026-05-23 16:55:58 +0000

    auto-commit: 2026m23-165558

--- yahtzee_backend.py
+++ yahtzee_backend.py
@@ -174,7 +174,7 @@ def score_chance(dice):
 def score_yahtzee(dice):
     score = 0
     dice_copy = dice[:]
-    sorted_
+    sorted_dice_set = sorted(set(dice))
     for i in range(1,7):
     return score
 
