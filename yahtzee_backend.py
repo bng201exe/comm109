71df4b51f88d054f09a42b1c0422997dbc78ec12
bng201exe <bng201@exeter.ac.uk>
2026-05-25 08:24:45 +0000

    auto-commit: 2026m25-082445

--- yahtzee_backend.py
+++ yahtzee_backend.py
@@ -209,7 +209,7 @@ def lock_dice(game_state, dice_to_keep):
 
 
 def toggle_dice(game_state, idx):
-    dice_to_toggle = int(idx)
+    #dice_to_toggle = int(idx)
     for die in game_state["locked"][dice_to_toggle]:
         if die == True:
             die == False
